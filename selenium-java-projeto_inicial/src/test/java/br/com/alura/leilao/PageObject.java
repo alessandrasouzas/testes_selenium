4befeb1cf9f77b511ec4c7a09ef45d6f5fc6776b
Alessandra Souza <le16bits@gmail.com>
2022-04-28 22:17:12 -0300

    configura diferentes tipos de Timeout da pagina

--- selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/PageObject.java
+++ selenium-java-projeto_inicial/src/test/java/br/com/alura/leilao/PageObject.java
@@ -1,5 +1,7 @@
 package br.com.alura.leilao;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -14,6 +16,11 @@ public class PageObject {
 		} else {
 			this.browser = new ChromeDriver();
 		}
+		
+		
+		this.browser.manage()
+			.timeouts().implicitlyWait(5, TimeUnit.SECONDS) //Tempo limite de espera para o selenium para buscar um elemento na página
+			.pageLoadTimeout(10, TimeUnit.SECONDS);			//Tempo limite de espera para o selenium para carregar a página
 	}
 
 	public void fechar() {
